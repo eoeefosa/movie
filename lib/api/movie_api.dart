9426393ae70e001ff70691fb18eb7317b62cd4b4
eoeefosa <eoeefosa@gmail.com>
2024-07-28 15:59:04 +0100

    updated api

--- lib/api/movie_api.dart
+++ lib/api/movie_api.dart
@@ -1,44 +1,24 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:torihd/models/movie.dart';
 
 class MovieApi {
   final firestore = FirebaseFirestore.instance;
 
-  Future fetchmovies() async {
-    firestore.collection('movies').get();
-  }
-
-  Future fetchTvseries() async {
-
-    try{
-
-    }on DioException catch (e, trace) {
-      debugPrint(e.response?.data.runtimeType.toString());
-      debugPrint(trace.toString());
-      throw Exception('Failed to fetch products: ${e.message}');
-    } catch (e, trace) {
-      debugPrint(e.toString());
-      debugPrint(trace.toString());
-      throw Exception('An unexpected error occurred: $e');
-    }
-    FirebaseFirestore.instance.collection('TV Series');
-  }
-
-  Future<List<dynamic>> fetchTrendingData() async {
+  Future<List<Movie>> fetchmovies() async {
     try {
-      // Fetch Trending Carousel data
-      final carouselSnapshot =
-          await firestore.collection('Trending Carousel').get();
-      final trendingCarousel =
-          carouselSnapshot.docs.map((doc) => doc.data()).toList();
+      final movies = await firestore.collection('movies').get();
+      final moviesdat = movies.docs.map((doc) {
+        final data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      }).toList();
 
-      // Fetch Top Picks data
-      final topPicksSnapshot = await firestore.collection('Top Picks').get();
-      final trendingContent =
-          topPicksSnapshot.docs.map((doc) => doc.data()).toList();
-      final trendingId = topPicksSnapshot.docs.map((doc) => doc.id).toList();
+      final List<Movie> movieslist = moviesdat.map((movie) {
+        return Movie.fromMap(movie);
+      }).toList();
 
-      return [trendingCarousel, trendingContent, trendingId];
+      return movieslist;
     } on FirebaseException catch (e) {
       debugPrint(e.toString());
       final trace = StackTrace.current;
@@ -47,15 +27,143 @@ class MovieApi {
       );
       debugPrint(trace.toString());
       throw Exception('FirebaseException: ${e.message}');
-
     } catch (e) {
       // Handle any other exceptions
       debugPrint(e.toString());
       final trace = StackTrace.current;
-    
       debugPrint(trace.toString());
       throw Exception('An unexpected error occurred: $e');
- 
     }
   }
+
+  Future fetchTvseries() async {
+    try {
+      final movies = await firestore.collection('TV Series').get();
+      final moviesdt = movies.docs.map((doc) {
+        final data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      }).toList();
+      final List<Movie> movieslist = moviesdt.map((movie) {
+        return Movie.fromMap(movie);
+      }).toList();
+
+      return movieslist;
+    } on FirebaseException catch (e) {
+      debugPrint(e.toString());
+      final trace = StackTrace.current;
+      debugPrint(
+        'FirebaseException: ${e.message}',
+      );
+      debugPrint(trace.toString());
+      throw Exception('FirebaseException: ${e.message}');
+    } catch (e) {
+      // Handle any other exceptions
+      debugPrint(e.toString());
+      final trace = StackTrace.current;
+      debugPrint(trace.toString());
+      throw Exception('An unexpected error occurred: $e');
+    }
+  }
+
+  Future<List> fetchTrendingCarousel() async {
+    try {
+      final movies = await firestore.collection('Trending Carousel').get();
+      final moviesdt = movies.docs.map((doc) {
+        final data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      }).toList();
+      final List<Movie> movieslist = moviesdt.map((movie) {
+        return Movie.fromMap(movie);
+      }).toList();
+
+      return movieslist;
+    } on FirebaseException catch (e) {
+      debugPrint(e.toString());
+      final trace = StackTrace.current;
+      debugPrint(
+        'FirebaseException: ${e.message}',
+      );
+      debugPrint(trace.toString());
+      throw Exception('FirebaseException: ${e.message}');
+    } catch (e) {
+      // Handle any other exceptions
+      debugPrint(e.toString());
+      final trace = StackTrace.current;
+
+      debugPrint(trace.toString());
+      throw Exception('An unexpected error occurred: $e');
+    }
+  }
+
+  Future<List> topPick() async {
+    try {
+      final movies = await firestore.collection('Top Picks').get();
+      final moviesdat = movies.docs.map((doc) {
+        final data = doc.data();
+        data['id'] = doc.id;
+        return data;
+      }).toList();
+
+      final List<Movie> movieslist = moviesdat.map((movie) {
+        return Movie.fromMap(movie);
+      }).toList();
+
+      return movieslist;
+    } on FirebaseException catch (e) {
+      debugPrint(e.toString());
+      final trace = StackTrace.current;
+      debugPrint(
+        'FirebaseException: ${e.message}',
+      );
+      debugPrint(trace.toString());
+      throw Exception('FirebaseException: ${e.message}');
+    } catch (e) {
+      // Handle any other exceptions
+      debugPrint(e.toString());
+      final trace = StackTrace.current;
+
+      debugPrint(trace.toString());
+      throw Exception('An unexpected error occurred: $e');
+    }
+  }
+
+  
+  // Future<List<dynamic>> fetchTrendingData() async {
+  //   try {
+  //     // Fetch Trending Carousel data
+  //     final carouselSnapshot =
+  //         await firestore.collection('Trending Carousel').get();
+  //     final trendingCarousel =
+  //         carouselSnapshot.docs.map((doc) => doc.data()).toList();
+
+  //     // Fetch Top Picks data
+  //     final topPicksSnapshot = await firestore.collection('Top Picks').get();
+  //     final trendingContent =
+  //         topPicksSnapshot.docs.map((doc) => doc.data()).toList();
+  //     final trendingId = topPicksSnapshot.docs.map((doc) => doc.id).toList();
+  //     final trendingContentresult = topPicksSnapshot.docs.map((doc) {
+  //       final data = doc.data();
+  //       data['id'] = doc.id;
+  //       return data;
+  //     }).toList();
+  //     return [trendingCarousel, trendingContent, trendingId];
+  //   } on FirebaseException catch (e) {
+  //     debugPrint(e.toString());
+  //     final trace = StackTrace.current;
+  //     debugPrint(
+  //       'FirebaseException: ${e.message}',
+  //     );
+  //     debugPrint(trace.toString());
+  //     throw Exception('FirebaseException: ${e.message}');
+  //   } catch (e) {
+  //     // Handle any other exceptions
+  //     debugPrint(e.toString());
+  //     final trace = StackTrace.current;
+
+  //     debugPrint(trace.toString());
+  //     throw Exception('An unexpected error occurred: $e');
+  //   }
+  // }
 }
---
+++ lib/models/movie.dart
@@ -0,0 +1,100 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+class Movie {
+  final String title;
+  final String downloadlink;
+  final String type;
+  final String movieImgurl;
+  final String rating;
+  final String id;
+  final String youtubetrailer;
+
+  Movie({
+    required this.title,
+    required this.downloadlink,
+    required this.type,
+    required this.movieImgurl,
+    required this.rating,
+    required this.id,
+    required this.youtubetrailer,
+  });
+
+  Movie copyWith({
+    String? title,
+    String? downloadlink,
+    String? type,
+    String? movieImgurl,
+    String? rating,
+    String? id,
+    String? youtubetrailer,
+  }) {
+    return Movie(
+      title: title ?? this.title,
+      downloadlink: downloadlink ?? this.downloadlink,
+      type: type ?? this.type,
+      movieImgurl: movieImgurl ?? this.movieImgurl,
+      rating: rating ?? this.rating,
+      id: id ?? this.id,
+      youtubetrailer: youtubetrailer ?? this.youtubetrailer,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'downloadlink': downloadlink,
+      'type': type,
+      'movieImgurl': movieImgurl,
+      'rating': rating,
+      'id': id,
+      'youtubetrailer': youtubetrailer,
+    };
+  }
+
+  factory Movie.fromMap(Map<String, dynamic> map) {
+    return Movie(
+      title: map['title'] as String,
+      downloadlink: map['downloadlink'] as String,
+      type: map['type'] as String,
+      movieImgurl: map['movieImgurl'] as String,
+      rating: map['rating'] as String,
+      id: map['id'] as String,
+      youtubetrailer: map['youtubetrailer'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Movie.fromJson(String source) => Movie.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'Movie(title: $title, downloadlink: $downloadlink, type: $type, movieImgurl: $movieImgurl, rating: $rating, id: $id, youtubetrailer: $youtubetrailer)';
+  }
+
+  @override
+  bool operator ==(covariant Movie other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.title == title &&
+      other.downloadlink == downloadlink &&
+      other.type == type &&
+      other.movieImgurl == movieImgurl &&
+      other.rating == rating &&
+      other.id == id &&
+      other.youtubetrailer == youtubetrailer;
+  }
+
+  @override
+  int get hashCode {
+    return title.hashCode ^
+      downloadlink.hashCode ^
+      type.hashCode ^
+      movieImgurl.hashCode ^
+      rating.hashCode ^
+      id.hashCode ^
+      youtubetrailer.hashCode;
+  }
+}
